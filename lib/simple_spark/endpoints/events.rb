09a09a636ab5697e5c02075a7083d5631240e8b3
Jono Warren <jono@numero.ai>
2023-11-08 19:31:14 -0800

    update

--- lib/simple_spark/endpoints/events.rb
+++ lib/simple_spark/endpoints/events.rb
@@ -27,12 +27,21 @@ module SimpleSpark
         @client.call(method: :get, path: 'events/message', query_values: params)
       end
 
-      # Perform a filtered search for message event data. The response is sorted by descending timestamp.
+      # Perform a filtered search for message event data.
+      # The result is not extracted, and the next cursor is parsed for the next api call.
       # @param params [String] Params to use in the search
       # @return [Array] a list of Event hash objects
       # @note https://developers.sparkpost.com/api/events/#events-get-search-for-message-events
-      def search_for_pagination(params = {})
-        @client.call(method: :get, path: 'events/message', query_values: params, extract_results: false)
+      def search_with_pagination(params = {})
+        result = @client.call(method: :get, path: 'events/message', query_values: params, extract_results: false)
+
+        # append cursor, to be passed into the next query
+        next_url = result.dig('links', 'next')
+        if next_url != nil
+          result['cursor'] = CGI.parse(URI(next_url).query)['cursor'].first
+        end
+
+        result
       end
     end
   end
